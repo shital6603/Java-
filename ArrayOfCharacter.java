4db645de286606e6d39d777d5e6b12927ca10c2b
Shital Khairnar <shital6603@gmail.com>
2023-11-12 15:56:14 +0530

    Created new java file

---
+++ ArrayOfCharacter.java
@@ -0,0 +1,11 @@
+
+public class ArrayOfCharacter {
+
+        public static void main(String[] args) {  
+            char[] arrayChar = {'h', 'e', 'j', 'y', 'k','a','c','d','s'};  
+            System.out.println("The array characters are as follows :");
+           for (int noOfCharacter = 0; noOfCharacter < arrayChar.length; noOfCharacter++) {
+                System.out.println(arrayChar[noOfCharacter]);
+           }
+        }  
+}
