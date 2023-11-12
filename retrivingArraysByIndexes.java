8ebf85cbb1c35d77bb89ddfb484fd04769f6473b
Shital Khairnar <shital6603@gmail.com>
2023-11-12 16:03:49 +0530

    Created new java file

---
+++ retrivingArraysByIndexes.java
@@ -0,0 +1,26 @@
+
+// Question : Declare an array and retrieve the value of array by its index.
+
+public class retrivingArraysByIndexes {
+
+
+        public static void main(String[] args) {
+         
+          int[] arrayOfIntegers = new int[5];
+      
+          
+          arrayOfIntegers[0] = 67;
+          arrayOfIntegers[1] = 90;
+          arrayOfIntegers[2] = 63;
+          arrayOfIntegers[3] = 29;
+          arrayOfIntegers[4] = 54;
+      
+       
+          System.out.println("First element of array :"+arrayOfIntegers[0]);
+          System.out.println("Second element of array :"+arrayOfIntegers[1]);
+          System.out.println("Third element of array :"+arrayOfIntegers[2]);
+          System.out.println("Forth element of array :"+arrayOfIntegers[3]);
+          System.out.println("Fifth element of array :"+arrayOfIntegers[4]);
+        }
+      
+}
