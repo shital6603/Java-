ffcf770ff6aab6be3b827d61bd0eb1e216716850
Shital Khairnar <shital6603@gmail.com>
2023-11-12 16:11:00 +0530

    New File Creation

---
+++ typeCastingLongToShort.java
@@ -0,0 +1,9 @@
+//Type cast a long datatype to short data type in java.
+
+public class typeCastingLongToShort {
+    public static void main(String[] args) {
+        long value1 =98645;
+        short typecastedvalue=(short)value1;
+        System.out.println(typecastedvalue);
+    }
+}
