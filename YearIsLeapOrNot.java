56dd2e18b74cd78cb2a12f5187fa769bf7cd4eaa
Shital Khairnar <shital6603@gmail.com>
2023-11-18 20:55:19 +0530

    updated leapyear file

--- YearIsLeapOrNot.java
+++ YearIsLeapOrNot.java
@@ -5,24 +5,24 @@ public class YearIsLeapOrNot {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a year :");
         int year=sc.nextInt();
-        if(year % 4 == 0)
-        {
-            if (year% 100 == 0) {
-                if (year % 400 == 0) {
-                    System.out.println("Year is leap year !!!");
-                }
-                else
+        if (year % 4  == 0) {
+            if (year % 100 == 0) 
+            {
+                if (year % 400 ==0) 
                 {
-                    System.out.println("Not leap year !!!");
+                    System.out.println("Leap year !!");
+                } else 
+                {
+                    System.out.println("Not leap year !!");
                 }
-            }
-            else{
+            } 
+            else 
+            {
                 System.out.println("Leap year !!");
             }
-        }
-        else{
-            System.out.println("Year is not leap year !");
-        }
 
+        } else {
+            System.out.println("Not leap year !!");
+        }
     }
 }
