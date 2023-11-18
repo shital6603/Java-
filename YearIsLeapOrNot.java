8dde874ca9e3707cdebd9fc9fe5fe288dd562880
Shital Khairnar <shital6603@gmail.com>
2023-11-18 20:39:36 +0530

    New file creation

---
+++ YearIsLeapOrNot.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class YearIsLeapOrNot {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a year :");
+        int year=sc.nextInt();
+        if(year % 4 == 0)
+        {
+            if (year% 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("Year is leap year !!!");
+                }
+                else
+                {
+                    System.out.println("Not leap year !!!");
+                }
+            }
+            else{
+                System.out.println("Leap year !!");
+            }
+        }
+        else{
+            System.out.println("Year is not leap year !");
+        }
+
+    }
+}
