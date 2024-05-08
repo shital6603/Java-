f0d7703ed6ca38c5ccb03d319bcd90ab0de682df
Shital Khairnar <78363561+shital6603@users.noreply.github.com>
2024-05-08 11:47:45 +0530

    Update Addition_of_two_numbers.java

--- Addition_of_two_numbers.java
+++ Addition_of_two_numbers.java
@@ -6,6 +6,6 @@ public static void main(String args[])
   int a=10;
 int b=20;
 int c=a+b;
-    System.out.println("Addition of 10 and 20 is :"+c)
+    System.out.println("Addition of 10 and 20 is :"+c);
   }
 }
